97d7a0010470cf8e4c2fddeb2e091bf4ba2f1fc1
NoyonAhmed <anoyon002@gmail.com>
2018-08-03 18:36:13 +0600

    Ending First Part for back end development

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-    console.log('Hello Full Stack Developer');
+app.get('/', (req, res, next) => {
+    res.send('<h1>hello world</h1>')
+    next()
 })
 
-app.listen
+app.get('/post', (req, res) => {
+    res.send('<h1>Hello, Full Stack Developer</h1>')
+})
+
+app.get('/user', (req, res) => {
+    res.send('<h1>Noyon Ahmed</h1>')
+})
+
+app.get('/user/email', (req, res) => {
+    res.send('<h1>email : anoyon002@gmail.com</h1>')
+})
+
+app.listen(PORT, () => {
+    console.log('Welcome Noyon Ahmed');
+})
